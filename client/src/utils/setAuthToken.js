98e7479c6c31a295789bc426208be903aba6f7d9
dxaginfo <142343166+dxaginfo@users.noreply.github.com>
2025-06-19 07:23:47 -0300

    Add auth token utility

---
+++ client/src/utils/setAuthToken.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+// Set or remove auth token in default headers for all axios requests
+export const setAuthToken = token => {
+  if (token) {
+    axios.defaults.headers.common['x-auth-token'] = token;
+    localStorage.setItem('token', token);
+  } else {
+    delete axios.defaults.headers.common['x-auth-token'];
+    localStorage.removeItem('token');
+  }
+};
